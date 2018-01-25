763c190cd026e0bb1f29660f91ab5688158f051d
Hydeia Blakey <blakeyhydeia@gmail.com>
2018-01-25 12:17:07 -0500

    Add changes to homepage styling index.ejs

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 var ejs = require('ejs');
 var path = require('path');//native module in express that resolves paths
-
+app.use(express.static('public'))
 var session = require('express-session');//this is authentication
 var morgan = require('morgan'); //middleware
-var mongoose = require('mongoose');//database
+// var mongoose = require('mongoose');//database
 
-var configDB = require('./config/database.js');
-mongoose.connect(configDB.url);
+// var configDB = require('./config/database.js');
+// mongoose.connect(configDB.url);
 
 
 app.use(morgan('dev')); //morgan logs to the console everytime a user send a request
@@ -21,16 +21,16 @@ app.set('view engine', 'ejs');
 app.listen(8080);
 console.log("Sever running on port 8080");
 
-app.use('/', function(req, res){
-	res.send('Our new app');
-	console.log('req.session');
-});
+// app.use('/', function(req, res){
+// 	res.send('Our new app');
+// 	console.log('req.session');
+// });
 
 
 // this is our homepage. 
-app.get('/', function(req, res) {
+app.get('/index', function(req, res) {
 	res.render('index.ejs')
-	// res.redirect('/signup')
+	res.redirect('/signup')
 
 });
 
