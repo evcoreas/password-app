99f0cd8b223db47d2c9bd879fd049d76d87bd99d
Chanice Collins <chanicecol@yahoo.com>
2018-01-25 18:49:01 -0500

    can sent users to the database through the form

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ var express = require('express');
 var app = express();
 var ejs = require('ejs');
 var path = require('path');//native module in express that resolves paths
-var bodyParser = require('body-parser');
 var session = require('express-session');//this is authentication
 var morgan = require('morgan'); //middleware
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose"); //to make mongo db work better
 
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/cat_app");
-// var mongoose = require('mongoose');//database
+//this connects mongoose to the mongo db database
+mongoose.connect("mongodb://localhost:27017/user_app");
 
-// var configDB = require('./config/database.js');
-// mongoose.connect(configDB.url);
 
 
 app.use(morgan('dev')); //morgan logs to the console everytime a user send a request
@@ -24,70 +22,55 @@ app.set('view engine', 'ejs');
 
 app.listen(8080);
 console.log("Sever running on port 8080");
-var catSchema = new mongoose.Schema({
-	name: String,
-	age: Number,
-	temperament: String
+
+var userSchema = new mongoose.Schema({
+	fname: String,
+	lname: String,
+	email: String
 });
 
-var Cat = mongoose.model("Cat", catSchema); //complied it to a model
+var User = mongoose.model("User", userSchema); //complied it to a model
 
-var george = new Cat({
-	name: "George",
-	age: 11,
-	temperment: "Grouchy"
-});
 
-george.save(function(err, cat){
-	if(err){
-		console.log("SOMETHIBNG WENT WRONG!")
-	} else {
-		console.log("We just saved a cat to the DB:")
-		console.log(cat);
-	}
+
+app.use('/index', function(req, res){
+	res.send('Our new app');
+	console.log('req.session');
 });
 
 
+// this is our homepage. 
+app.get('/index', function(req, res) {
+	res.render('index.ejs')
 
+});
 
+//page number 2
+app.get( '/mainpage', function(req, res) {
+	res.render('mainpage.ejs')
 
+});
 
-// app.use('/index', function(req, res){
-// 	res.send('Our new app');
-// 	console.log('req.session');
-// });
+//signp page render
+app.get('/signup', function(req, res) {
+	res.render('signup.ejs')
+});
 
+app.post('/newUser', function(req,res){
+	User.create({
+		fname: req.body.firstName,
+		lname: req.body.lastName,
+		email: req.body.email
+	})
+	res.redirect('/users')
 
-// // this is our homepage. 
-// app.get('/index', function(req, res) {
-// 	res.render('index.ejs')
-// 	// res.redirect('/signup')
-
-// });
-
-// //page number 2
-// app.get( '/mainpage', function(req, res) {
-// 	res.render('mainpage.ejs')
-
-// });
-
-// //page number3
-// // app.use('/signup', function(req, res){
-// // 	res.render('signup.ejs')
-// // });
-// app.get('/signup', function(req, res) {
-// 	res.render('signup.ejs')
-// });
-
-// app.post('/newUser', function(req,res){
+})
+app.get('/users', function(req,res){
+	//get the users from the data base
+	 var user = User.find({}, function(err, user){
+	//renders the user file 
+	res.render('users.ejs', {user: user}) //user on the left is the variable name in ejs file 
+										 //user on the right is the varibale name in this file 
+	});
 	
-// 		req.body.firstName,
-// 		lname: req.body.lastName,
-// 		email: req.body.email
-// 	}
-// 	res.redirect('/users')
-
-// })
-// app.get('/users', function(req,res){
-// 	res.render('users.ejs')
-// })
+});
