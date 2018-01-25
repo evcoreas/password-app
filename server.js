8361ccf37838d73572b9b6a9c1b5b68793d3bbd9
evcoreas <evcoreas@live.com>
2018-01-24 21:15:04 -0500

    removed pug and added ejs

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var app = express();
-var pug = require('pug');
+var ejs = require('ejs');
+var path = require('path');//native module in express that resolves paths
+
 var session = require('express-session');//this is authentication
 var morgan = require('morgan'); //middleware
 var mongoose = require('mongoose');//database
 
 var configDB = require('./config/database.js');
-mongoos.connect(configDB.url);
+mongoose.connect(configDB.url);
 
 
-app.use(morgan('dev')); //morgan logs to the cosole everytime a user send a request
+app.use(morgan('dev')); //morgan logs to the console everytime a user send a request
 app.use(session({secret:'anysecret',
 				saveUninitialized: true,
 				resave: true}));
 
-app.set('view engine', 'pug')
+app.set('view engine', 'ejs');
 
 app.listen(8080);
 console.log("Sever running on port 8080");
